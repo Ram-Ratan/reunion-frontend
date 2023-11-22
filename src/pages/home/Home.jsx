a110ab6b5daae6037a7d580aff78366b59a4866b
Ram Singh <ram.singh@talentmonk.com>
2023-11-23 02:11:53 +0530

    responsive fixes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -39,7 +39,7 @@ const Home = () => {
   },[])
 
   return (
-    <div className="mx-[100px] flex flex-col mt-10 gap-6">
+    <div className="mx-[10px] md:mx-[100px] flex flex-col mt-10 gap-6">
       <div className="w-full">
         <input
           type="text"
